36767cc79c5f59ecb92127a7e52b5278bed68a66
rachitpoudel101 <rachit300poudel@gmail.com>
2024-03-05 12:27:57 -0500

    learn about theDjango Templets and add two numbers using DTL.

--- playground/views.py
+++ playground/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def say_hello(request):
     # return HttpResponse('Hello I am Rachit Poudel')
     return render(request, 'hello.html',{'address':'from KTM'})
+
+def add(request):
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    res = val1 + val2
+    
+    
+    return render(request,'result.html',{'result':res})
