ca57bae3c613fa786dc59e7ff2b2581d8d5866ae
rachitpoudel101 <rachit300poudel@gmail.com>
2024-03-06 11:00:33 -0500

    learn about the post method and static file use in django.

--- frontend/urls.py
+++ frontend/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/',include('playground.urls'))
-]
+    path('playground/',include('playground.urls')),
+    path('travello/',include('travello.urls'))
+]   
